6e720cdad7e4ee141e2ac21e1e984390b79f2ca9
OsAlansari <149081443+OsAlansari@users.noreply.github.com>
2024-11-24 21:55:29 +0300

    can't figure out how to make a table with the datadata🫠🫠

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 $URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 $response = file_get_contents($URL);
 $result = json_decode($response, true);
-var_dump($result)
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,7 +14,7 @@ var_dump($result)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>UOB Data API</title>
 </head>
 <body>
     
